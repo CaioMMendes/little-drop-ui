40fd45bfab70b328dad41e99016da88fad25113a
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-02-13 17:13:34 -0300

    chore: Add types exports

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -7,3 +7,21 @@ export * from "./components/Avatar";
 export * from "./components/Input";
 export * from "./components/TextArea";
 export * from "./components/Checkbox";
+
+export type { AvatarProps } from "./components/Avatar";
+export type { BoxProps } from "./components/Box";
+export type { ButtonProps } from "./components/Button";
+export type { CheckboxProps } from "./components/Checkbox";
+export type { HeadingProps } from "./components/Heading";
+export type {
+  InputProps,
+  InputContainerProps,
+  InputLabelProps,
+  InputLabelTextProps,
+} from "./components/Input";
+export type {
+  TextAreaProps,
+  TextAreaContainerProps,
+  TextAreaLabelProps,
+  TextAreaLabelTextProps,
+} from "./components/TextArea";
